65347032d84a2b84020f1b691b5c9b1987b96f3c
Aaron Whitehead <whitehead.aaron@live.com>
2018-09-15 16:47:41 -0700

    finished about me page event listening and rendering of the DOM

--- Public/Scripts/eventHandler.js
+++ Public/Scripts/eventHandler.js
@@ -8,7 +8,28 @@ const eventHandlers = (function() {
     let output = '';
 
     if (store.html === 'aboutMe') {
-      output = '<h2>About Me</h2>';
+      output =
+       `<div class="aboutMeGrid">
+       <div class = "aboutHeader">
+        <h2>Randy Whitehead</h2>
+            <h3>CPA</h3>
+    </div>
+    <img src="../Images/randy.jpg" class="aboutMeImage">
+    <div class="aboutInfo">
+        <p>My name is Randy, and I have been running my own tax business for the people in the greater Madison area for 10? years. My full time day job is working for the City of Madison as a Principal Accountant. I have been working in this role since March of 2004.</p>
+    </div>
+    <div class="aboutQualificationsHeader">
+        <h2>Qualifications</h2>
+    </div>
+    <div class="aboutQualificationCPA">
+        <h3>Certified Public Account</h3>
+            <p>Some information about your CPA certification</p>
+    </div>
+    <div class="aboutQualificationCollege">
+        <h3>Upper Iowa University</h3>
+            <p>Some information about your college</p>
+    </div>
+    </div>`;
     }
     if (store.html === 'contactMe') {
       output = '<h2>Contact Me</h2>';
@@ -20,9 +41,10 @@ const eventHandlers = (function() {
   }
 
   function render() {
-    
+    let pageStuff = aboutMeHTML();
+    console.log(pageStuff);
 
-    $('.stuff').html(aboutMeHTML);
+    $('.replaceMe').html(pageStuff);
   }
 
 
