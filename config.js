f764517fc1fa57af13e7f5ec818956e312bbf348
Aaron Whitehead <whitehead.aaron@live.com>
2018-09-24 19:13:15 -0700

    test

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/whitehead-tax-service',
-  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost:27017/whitehead-tax-service'
+  MONGODB_URI: process.env.MONGODB_URI 
+  // || 'mongodb://localhost:27017/whitehead-tax-service',
+  // TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost:27017/whitehead-tax-service'
 };
